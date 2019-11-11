b0f5522a0d3667c190b16c0b7a98ab3896173508
Peter Gal <galpeter@inf.u-szeged.hu>
2019-11-11 16:30:15 +0100

    [BLE] Add missing header guard

--- source/blue.hpp
+++ source/blue.hpp
@@ -4,6 +4,8 @@
 // <LICENSE.md or https://opensource.org/licenses/BSD-3-Clause>.
 // This file may not be copied, modified, or distributed except
 // according to those terms.
+#ifndef BLUE_HPP
+#define BLUE_HPP
 
 #include <ble/BLE.h>
 
@@ -11,3 +13,4 @@ void blue_AdvertisementCallback(const Gap::AdvertisementCallbackParams_t* params
 void blue_StartScan(BLE& ble);
 void blue_AddConnectionCallbacks(BLE& ble);
 
+#endif
